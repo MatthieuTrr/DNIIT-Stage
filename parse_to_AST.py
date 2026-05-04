adc950ab60585d310d2d92c7d82ba8701d311710
MatthieuTrr <terrier.matthieu.41@gmail.com>
2026-05-04 05:06:40 +0200

    Update parse_to_AST.py

--- parse_to_AST.py
+++ parse_to_AST.py
@@ -10,3 +10,33 @@ def parse_java_to_ast_vectors(java_source_code):
             vi.append(node_type)
         vectors.append(vi)
     return vectors
+
+def test_ast():
+    java_program_class_basic="""
+    public class Point2D {
+    int x;
+    int y;
+    }
+    """
+    java_program_class_if_statement="""
+    public class Loop{
+    int a;
+    public static void if_statement(boolean B){
+        if (B==true){
+            a=2;
+            }
+        else{
+            a=1;
+            }
+        }
+    }
+    """
+    vectors_basic=parse_java_to_ast_vectors(java_program_class_basic)
+    vectors_if=parse_java_to_ast_vectors(java_program_class_if_statement)
+    print(f"Vecteurs de vectors_basic : ")
+    for v in vectors_basic:
+        print(v)
+    print(f"Vecteurs de vectors_if : ")
+    print(f"(On devrait voir la boucle if)")
+    for v in vectors_if:
+        print(v)
